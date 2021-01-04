f991525615490c6b454332c4af15aee6dfc8e06d
Paweł Burgchardt <pawel.burgchardt@blackburst.pl>
2021-01-04 10:52:07 +0100

    receiving yagna events

--- src/components/chessDashboard.jsx
+++ src/components/chessDashboard.jsx
@@ -4,7 +4,7 @@ import socket from "./../services/socketService";
 import { toast } from "react-toastify";
 class ChessDashboard extends Component {
 
-
+    state = { fen: "start", statusBar:"" };
    
 
     componentDidMount() {
@@ -12,7 +12,26 @@ class ChessDashboard extends Component {
         socket.on("positionEvent", this.handlePositionEvent);
         socket.on("moveEvent", this.handleMoveEvent);
         socket.on("workerFailed", this.handleWorkerFailed);
-      }
+        socket.on("agreementCreated", this.handleAgreementCreated);
+        socket.on("agreementConfirmed", this.handleAgreementConfirmed);
+        socket.on("computationFinished", this.handleComputationFinished);
+      };
+      handleComputationFinished = (params) => {
+        const {taskId,time} = params;
+        let timeInSec = Math.round(time/1000);
+        toast.info("computation finished in : "+time+"ms  (~ "+timeInSec+"s )");
+      }; 
+      handleAgreementConfirmed = (params) => {
+        const {taskId,workerName} = params;
+        console.log("agreement confirmed...");
+        console.log(params);          
+        this.setState({ statusBar:"agreement confirmed taskId: " + taskId + " / worker: "+workerName });
+      }; handleAgreementCreated = (params) => {
+        const {taskId,workerName} = params;
+        console.log("agreement created...");
+        console.log(params);          
+        this.setState({ statusBar:"agreement created taskId: " + taskId + " / worker: "+workerName });
+      };
       handlePositionEvent = (params) => {
         console.log("position event...");
         console.log(params);          
@@ -20,11 +39,12 @@ class ChessDashboard extends Component {
         console.log("chess pos", fen);
         this.setState({ fen });
       };
-      handleMoveEvent = ({move}) => {
+      handleMoveEvent = (bestmove) => {
+          const {move,depth,time}=bestmove;
         console.log("move event...");
         console.log(move);          
         if(move!=="test")
-        toast.info("move : "+move)
+        toast.info("move : "+move +"\ntime: "+time);
       };
       handleWorkerFailed = ({worker}) => {
         console.log("worker failed...");
@@ -32,9 +52,9 @@ class ChessDashboard extends Component {
         if(worker!=="test")
         toast.error("worker failed : "+worker)
       };
-    state = { fen: "start" };
+   
     render() { 
-        return ( <div><Chessboard
+        return ( <div><h1>{this.state.statusBar}</h1><Chessboard
             width={1024}
             id="random"
             position={this.state.fen}
