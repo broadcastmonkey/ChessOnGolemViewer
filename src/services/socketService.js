5c3c83723cb1c2e60854320da174d39a94d26810
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-02-22 17:44:50 +0100

    debugging current env var

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,7 +1,10 @@
 import io from "socket.io-client";
 
+const NAME = process.env.REACT_APP_NAME;
 const ENDPOINT = process.env.REACT_APP_SOCKET_SERVER_URL;
-console.log("end point : " + ENDPOINT);
+console.log(NAME + " // end point : " + ENDPOINT);
+console.log(node.env);
+console.log(node.env.NODE_ENV);
 const socket = io(ENDPOINT);
 
 export default socket;
