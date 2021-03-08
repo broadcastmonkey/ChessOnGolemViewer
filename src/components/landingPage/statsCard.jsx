929025fa52babcba0cf5d45b7ac3de3d89db412c
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-03-08 13:46:00 +0100

    requesting new golem vs golem game

--- src/components/landingPage/statsCard.jsx
+++ src/components/landingPage/statsCard.jsx
@@ -7,7 +7,7 @@ import GameIcon from "./../gameIcon";
 import { Link } from "react-router-dom";
 
 class StatsCard extends Component {
-    renderGolemVsGolemInfo = (isActive, gameId) => {
+    renderGolemVsGolemInfo = (isActive, gameId, requestGameCallback) => {
         if (isActive) {
             return (
                 <React.Fragment>
@@ -23,7 +23,7 @@ class StatsCard extends Component {
         return (
             <React.Fragment>
                 At the moment no golem vs golem game is happening...
-                <Button className="ml-4" variant="primary">
+                <Button className="ml-4" variant="primary" onClick={requestGameCallback}>
                     Request it now
                 </Button>
             </React.Fragment>
@@ -37,6 +37,7 @@ class StatsCard extends Component {
             playerVsGolemGamesCount,
             isGolemVsGolemActive,
             golemVsGolemGameId,
+            requestGameCallback,
         } = this.props;
 
         return (
@@ -57,7 +58,11 @@ class StatsCard extends Component {
                         <br />- {playerVsGolemGamesCount} <i>human vs golem</i> games{" "}
                         <GameIcon type={GameType.PlayerVsGolem} />
                         <hr />
-                        {this.renderGolemVsGolemInfo(isGolemVsGolemActive, golemVsGolemGameId)}
+                        {this.renderGolemVsGolemInfo(
+                            isGolemVsGolemActive,
+                            golemVsGolemGameId,
+                            requestGameCallback,
+                        )}
                         <br />
                         <hr />
                         <br />
